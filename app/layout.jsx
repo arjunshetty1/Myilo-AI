e043be81c4d843b7adad986b6c90ac3fb2ab9dbc
arjunshetty1 <arjunjayapura@gmail.com>
2025-02-15 00:13:03 +0530

    ADD: Vercel Speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ClientLayout>
-          {children} <Analytics />
+          {children} <Analytics /> <SpeedInsights />
         </ClientLayout>
       </body>
     </html>
