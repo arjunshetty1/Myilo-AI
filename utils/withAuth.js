6aa4e031ba890553366ac96cf3c8f4d954d4bf11
arjunshetty1 <arjunjayapura@gmail.com>
2025-02-20 00:34:55 +0530

    revert auth

--- utils/withAuth.js
+++ utils/withAuth.js
@@ -64,7 +64,7 @@ export function withAuth(WrappedComponent) {
             "/contact-us",
             "/credits",
             "/privacy-policy",
-            "/select-template"
+            
           ].includes(pathname);
           
           const isPublicPattern = [
